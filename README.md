fe254364fbedcc035fb42299dc76c29b41b613f6
홍계준(Kyejun Hong) <kyejune@korea.ncsoft.corp>
2018-08-28 16:36:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# ts-sample
+
+> Nuxt.js TypeScript project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+## Build Flow
+```
+1. install nuxt with typescript
+   : https://github.com/nuxt-community/typescript-template
+
+2. install ssss
+   : https://nuxtjs.org/api/configuration-css
+```
+
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <h2>{{ `${person.id}+${dataExample}` }}</h2>
+      <p>{{ objExample.name }}
+      </p>
+      <img :src="'https://robohash.org/' + person.first_name + '_' + person.last_name" />
+      <div>
+        <button @click="changeData">change data</button>
+        <button @click="changeComplexObject">change complex object</button>
+      </div>
+   </div>
+</template>
+
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue
+} from "nuxt-property-decorator"
+
+
+
+type ComplexObjectInterfacce = {
+    name: string
+    no: number
+}
+
+
+
+@Component({})
+export default class Card extends Vue {
+    @Prop() person
+
+    dataExample: string = 'This Property Will Be Data'
+    objExample: ComplexObjectInterfacce = { no:300, name:'nnnn' }
+
+
+    // get computedExample() {
+    //     return this.propExample.no + this.propExample.name;
+    // }
+
+
+    changeData() {
+        this.dataExample = "This is being done in a method"
+    }
+
+    changeComplexObject(){
+        this.objExample.name = 'new nnnn'
+    }
+
+
+
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    padding: 0;
+    margin: 0.25rem;
+    border: 0.25rem solid gainsboro;
+
+    img{
+         background-color: gray;
+        margin: 1em;
+     }
+}
+</style>
